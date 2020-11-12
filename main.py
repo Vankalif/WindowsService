ff6c47e8ae4f7bf81f438a5e16aae132458a9a5a
Nicholas <vankalif@gmail.com>
2020-11-12 16:59:20 +0300

    dev commit

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-import socket
 import time
-from datetime import datetime
-
-import servicemanager
-import win32event
-import win32service
-import win32serviceutil
-import getpass
 import logging
 import graypy
-
+import getpass
 from typing import Optional
 from ctypes import windll, create_unicode_buffer
+
 
 SEARCHING_NAME = 'консультант'
 
@@ -32,7 +25,7 @@ class ConsFilter(logging.Filter):
 
 conslogger = logging.getLogger("ConsWatchDogLogger")
 conslogger.setLevel(logging.DEBUG)
-handler = graypy.GELFUDPHandler("172.16.0.39", 12206)
+handler = graypy.GELFUDPHandler("172.16.0.39", 12206, debugging_fields=False)
 conslogger.addHandler(handler)
 conslogger.addFilter(ConsFilter())
 
@@ -63,65 +56,10 @@ def counter(hwnd: int) -> int:
     return active_time
 
 
-def write_to_file():
-    with open("C:\\Users\\turshiev_nm\\Desktop\\WindowsService\\service.log", "a+") as file:
-        file.write(str(datetime.now()) + "\n")
-
-
-class ServiceBaseClass(win32serviceutil.ServiceFramework):
-
-    _svc_name_ = 'pythonService'
-    _svc_display_name_ = 'Python Service'
-    _svc_description_ = 'Python Service Description'
-
-    @classmethod
-    def parse_command_line(cls):
-        win32serviceutil.HandleCommandLine(cls)
-
-    def __init__(self, args):
-        win32serviceutil.ServiceFramework.__init__(self, args)
-        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
-        socket.setdefaulttimeout(60)
-
-    def SvcStop(self):
-        self.stop()
-        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        win32event.SetEvent(self.hWaitStop)
-
-    def SvcDoRun(self):
-        self.start()
-        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
-                              servicemanager.PYS_SERVICE_STARTED,
-                              (self._svc_name_, ''))
-        self.main()
-
-    def start(self):
-        pass
-
-    def stop(self):
-        pass
-
-    def main(self):
-        pass
-
-
-class ExampleService(ServiceBaseClass):
-    _svc_name_ = "ConsWatchDogService"
-    _svc_display_name_ = "Сервис мониторинга активности"
-    _svc_description_ = "Сервис мониторинга активности Консультант+"
-
-    def start(self):
-        self.isrunning = True
-
-    def stop(self):
-        self.isrunning = False
-
-    def main(self):
-        while self.isrunning:
-            while self.isrunning:
-                write_to_file()
-                time.sleep(5)
-
-
-if __name__ == '__main__':
-    ExampleService.parse_command_line()
+while True:
+    hwnd = get_foreground_window_HWND()
+    hwnd_window_name = getForegroundWindowTitle(hwnd)
+    if SEARCHING_NAME in hwnd_window_name:
+        active_time = counter(hwnd)
+        graylog_it(active_time)
+    time.sleep(5)
